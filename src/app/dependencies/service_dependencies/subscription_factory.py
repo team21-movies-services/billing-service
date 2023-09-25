64d0f48a4357d2a890520dab3952bc21985f9a40
Орлов Ярослав <y.orlov@fonmix.ru>
2023-09-25 22:31:47 +0300

    features/yookassa: add subscription draft

--- src/app/dependencies/service_dependencies/subscription_factory.py
+++ src/app/dependencies/service_dependencies/subscription_factory.py
@@ -1,16 +1,14 @@
-from fastapi import Depends
-
 from app.dependencies.registrator import add_factory_to_mapper
 from app.dependencies.repository_dependencies.user_subscription import (
     SubscriptionRepositoryDep,
 )
+from app.dependencies.service_dependencies.tariff_factory import TariffServiceDep
 from app.services.subscription import SubscriptionService, SubscriptionServiceABC
-from app.services.tariff import TariffServiceABC
 
 
 @add_factory_to_mapper(SubscriptionServiceABC)
 def create_subscription_service(
     subscription_repository: SubscriptionRepositoryDep,
-    tariff_service: TariffServiceABC = Depends(),
+    tariff_service: TariffServiceDep,
 ) -> SubscriptionService:
     return SubscriptionService(tariff_service, subscription_repository)
