55fcc5009a5040dfd5f9a3b569502c2dbffc6eca
Leonid Bakhmetev <leonidbkh@pm.me>
2023-10-01 18:48:09 +0400

    chore:

--- src/worker/core/config.py
+++ src/worker/core/config.py
@@ -1,8 +1,5 @@
-from typing import Any
-
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
-from yookassa import Configuration
 
 
 class WorkerSettings(BaseSettings):
@@ -31,12 +28,7 @@ class YookassaConfig(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="YOOKASSA_")
 
     api_key: str = Field(default="")
-    base_url: str = Field(default="https://api.yookassa.ru/v3/")
     shop_id: str = Field(default="")
-
-    def __init__(self, **values: Any):
-        super().__init__(**values)
-        Configuration.configure(self.shop_id, self.api_key)
 
 
 class Settings(BaseSettings):
--- src/worker/main.py
+++ src/worker/main.py
@@ -3,20 +3,30 @@ from datetime import timedelta
 from time import sleep
 
 from scheduler.threading.scheduler import Scheduler
-from worker.container import app
-from worker.core.config import Settings
+from worker.container import app, settings
 from worker.core.logger import Logger
 from worker.services.payment import PaymentStatusService
+from yookassa import Configuration
 
 logging.config.dictConfig(app.resolve(Logger).get_settings())
 logger = logging.getLogger(__name__)
 
 
-if __name__ == '__main__':
+PENDING_PAYMENTS_CHECK_INTERVAL = timedelta(seconds=settings.worker.pending_payments_check)
+
+
+# Конфигурация SDK Юкассы
+Configuration.configure(settings.yookassa.shop_id, settings.yookassa.api_key)
+
+
+def main():
     scheduler = Scheduler()
-    settings = app.resolve(Settings)
-    service: PaymentStatusService = app.resolve(PaymentStatusService)
-    scheduler.cyclic(timedelta(minutes=settings.worker.pending_payments_check), service.update_pending_payments)
+    payment_service: PaymentStatusService = app.resolve(PaymentStatusService)
+    scheduler.cyclic(PENDING_PAYMENTS_CHECK_INTERVAL, payment_service.update_pending_payments)
     while True:
         scheduler.exec_jobs()
         sleep(1)
+
+
+if __name__ == '__main__':
+    main()
