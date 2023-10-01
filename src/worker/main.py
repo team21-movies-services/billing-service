8b66424699af70357806fc20118eb89d4669dad6
Leonid Bakhmetev <leonidbkh@pm.me>
2023-10-01 18:59:17 +0400

    chore:

--- src/worker/main.py
+++ src/worker/main.py
@@ -6,17 +6,12 @@ from scheduler.threading.scheduler import Scheduler
 from worker.container import app, settings
 from worker.core.logger import Logger
 from worker.services.payment import PaymentStatusService
-from yookassa import Configuration
 
 logging.config.dictConfig(app.resolve(Logger).get_settings())
 logger = logging.getLogger(__name__)
 
 
 PENDING_PAYMENTS_CHECK_INTERVAL = timedelta(seconds=settings.worker.pending_payments_check)
-
-
-# Конфигурация SDK Юкассы
-Configuration.configure(settings.yookassa.shop_id, settings.yookassa.api_key)
 
 
 def main():
--- src/worker/providers/yookassa.py
+++ src/worker/providers/yookassa.py
@@ -1,14 +1,18 @@
 import logging
 
+from worker.core.config import Settings
 from worker.providers.base_provider import BasePaymentProvider
 from worker.schemas.payment import PaymentSchema
-from yookassa import Payment
+from yookassa import Configuration, Payment
 from yookassa.client import NotFoundError
 
 logger = logging.getLogger(__name__)
 
 
 class YookassaPaymentProvider(BasePaymentProvider):
+    def __init__(self, settings: Settings):
+        Configuration.configure(settings.yookassa.shop_id, settings.yookassa.api_key)
+
     def get_payment_status(self, payment: PaymentSchema) -> PaymentSchema | bool:
         """Get info from remote payment provider"""
         logger.debug("Checking updates for payment %s", payment.id)
