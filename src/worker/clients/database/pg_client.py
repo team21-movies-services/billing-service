02c5791bef6eab7aa1eb33b9116d31a576b9f812
Leonid Bakhmetev <leonidbkh@pm.me>
2023-10-01 18:06:38 +0400

    fix: async provider deleted

--- src/worker/clients/database/pg_client.py
+++ src/worker/clients/database/pg_client.py
@@ -1,39 +1,11 @@
-from contextlib import asynccontextmanager, contextmanager
-from typing import AsyncGenerator, Generator
+from contextlib import contextmanager
+from typing import Generator
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import Session, sessionmaker
 from worker.core.config import Settings
 
-__all__ = ["AsyncSQLAlchemyProvider", "SQLAlchemyProvider"]
-
-
-class AsyncSQLAlchemyProvider:
-    def __init__(
-        self,
-        settings: Settings,
-    ):
-        self.async_dsn = settings.postgres.database_url
-        self.echo_log = settings.postgres.echo_log
-        self.async_engine = create_async_engine(
-            self.async_dsn,
-            echo=self.echo_log,
-            max_overflow=20,
-            pool_size=10,
-        )
-        self.async_session_maker = async_sessionmaker(
-            self.async_engine,
-            expire_on_commit=False,
-            class_=AsyncSession,
-            autocommit=False,
-            autoflush=False,
-        )
-
-    @asynccontextmanager
-    async def get_session(self) -> AsyncGenerator[AsyncSession, None]:
-        async with self.async_session_maker() as session:
-            yield session
+__all__ = ["SQLAlchemyProvider"]
 
 
 class SQLAlchemyProvider:
