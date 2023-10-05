062db28a577335e0098949027aad00f8522eb5da
Pavel Khramov <102523130+KhramovKhramov@users.noreply.github.com>
2023-10-05 23:15:27 +0500

    feautre: event_service in subscription service

--- src/worker/services/subscription.py
+++ src/worker/services/subscription.py
@@ -1,6 +1,8 @@
 import logging
 from dataclasses import dataclass
 
+from shared.constants import EventTypes
+from shared.services import EventSenderService
 from worker.core.config import Settings
 from worker.providers import ProviderFactory
 from worker.uow import UnitOfWorkABC
@@ -13,11 +15,18 @@ class SubscriptionService:
     _provider_factory: ProviderFactory
     _settings: Settings
     _uow: UnitOfWorkABC
+    _event_service: EventSenderService
 
     def disable(self):
         with self._uow:
             subs = self._uow.subscription_repo.disable()
             self._uow.commit()
-            # TODO: отпарвка события о деактивации подписки
+            for sub in subs:
+                cancel_event_data = {
+                    "user_id": sub.user_id,
+                    "sub_id": sub.id,
+                    "tariff_id": sub.tariff_id,
+                }
+                self._event_service.send_event(event_type=EventTypes.CancelSubscripton, data=cancel_event_data)
             logger.info("Subscriptions with ids %s disabled.", ", ".join(str(sub.id) for sub in subs))
         logger.info("Subscriptions disable task complete")
